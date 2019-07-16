f2d3ecda19d0762c81df41f10f6e4b2340c3042d
Kumar <8055rk@gmail.com>
2019-07-15 21:20:50 -0400

    unassigned shards commands

--- unassigned_shards.sh
+++ unassigned_shards.sh
@@ -1,1 +1,5 @@
+#command to print unassigned shards
+curl http://master node: port/_cat/shards?h=index,shard,prirep,state,unassigned.reason | grep UNASSIGNED | awk '{print $1}'
+
+#command to get the indices list along the httplink
 curl http://master node: port/_cat/shards?h=index,shard,prirep,state,unassigned.reason | grep UNASSIGNED | awk '{print "http://master node: port/" $1}'
